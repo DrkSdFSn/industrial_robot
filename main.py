5bb5640f0e6f7858e8268d01f7e3bfa2b80cc27b
Alexander Osin <alexandre.osin@gmail.com>
2016-02-02 10:29:51 +0200

    add func draw_graph_create_module

--- main.py
+++ main.py
@@ -1,51 +1,67 @@
 from gks import count_unique, count_diff, matrix_output, grouped, gks3, unique_module
 from gks4 import draw_graph_create_module
+from outline import lab_6_7
 from os import stat, path
 from sys import argv
+from copy import deepcopy
 
-p = []
+matrix_opr = []
 condition = True
-filepath = str(path.realpath(path.dirname(argv[0]))) + '\matrix.txt'
+filepath = str(path.realpath(path.dirname(argv[0]))) + '\matrix.txt' # if file is empty, input via the console
 if stat(filepath).st_size == 0:
 	condition = False
-	n = int(input('Введите количество строк операций: ')) 
+	n = int(input('Enter the number of lines of operations: '))
 	for i in range(n):
 		print(str(i + 1)+': ', end='')
-		p.append([j for j in input().split()])
+		matrix_opr.append([j for j in input().split()])
 if condition:
 	for line in open(filepath):
-		p.append( [x for x in line.split() ] )
+		matrix_opr.append( [x for x in line.split() ] )
 
-#вывод входных данных
-print('Входные данные: ')
-for i in range(len(p)):
-    for j in range(len(p[i])):
-        print(p[i][j], end=' ')
+#input matrix of operations
+print('Input data: ')
+for i in range(len(matrix_opr)):
+    for j in range(len(matrix_opr[i])):
+        print(matrix_opr[i][j], end=' ')
     print()
 
-n = count_unique(p)
+nn = count_unique(matrix_opr) 
+n = int(len(nn)/2)
+print("\nCount of unique elements: ", n, "(", nn,")")
 
-lens = len(p)
-ss2 = count_diff(p, n, lens)
-m = matrix_output(ss2, lens)
-mg = grouped(m.tolist())
-specify_group = gks3(mg, p)
+lens = len(matrix_opr)
+ss2 = count_diff(matrix_opr, n, lens)
+
+matrix_out = matrix_output(ss2, lens)
+print('\nSquare matrix:\n', matrix_out, sep='')
+
+groups = grouped(matrix_out.tolist())
+print('\nGroups:')
+for i in groups.items():
+	#print('\t', i[0], 'group:', i[1],  set([k for j in i[1] for k in matrix_opr[j]]))
+	print("\t {0:<1d} group: {1!s:<30s} -> {2!s:<100s}".format(i[0], i[1], set([k for j in i[1] for k in matrix_opr[j]])))
+
+specify_group1 = gks3(groups, matrix_opr) # some bug with this fubction
+refin_groups = gks3(groups, matrix_opr) # for it works well we must call twice this func
+print('\nRefined groups:')
+for i in refin_groups.items():
+	#print('\t', i[0], 'group:', i[1], set([k for j in i[1] for k in matrix_opr[j]]))
+	print("\t {0:<1d} group: {1!s:<35s} -> {2!s:<100s}".format(i[0], i[1], set([k for j in i[1] for k in matrix_opr[j]])))
+    	
 module = {}
-for i in specify_group.keys():
-	module[i] = draw_graph_create_module(p, specify_group[i], 'group'+str(i))
-un_mod = unique_module(module)
-
-print('\nВыходные данные:\n', m, sep='')
-print('\nГруппы:')
-for i in mg.items():
-	print(i[0], 'group:', i[1])
-print('\nУточненные группы:')
-for i in specify_group.items():
-	print(i[0], 'group:', i[1])
-print('\nМодули:')
+for i in refin_groups.keys():
+	module[i] = draw_graph_create_module(matrix_opr, refin_groups[i], 'group'+str(i))
+print('\nModules:')
 for i in module.items():
-	print(i[0], 'group:', i[1])
-print('\nУникальные модули:')
+	print('\t', i[0], 'group:')
+	for j in i[1].items():
+		print('\t', j[0], j[1])
+	print('\n')
+
+un_mod = unique_module(module)
+print('\nRefined modules:')
 for i in un_mod.items():
-	print(i[0], ':', i[1])
-input("\nДля выхода нажмите любую клавишу")
+	print('\t', i[0], ':', i[1])
+	
+lab_6_7(matrix_opr, un_mod)
+input("\nTo exit, press any key")
